fedd8b543020240802c8d3d5617d25d25dfa4899
dvzin0770-rgb <dvzin0770@gmail.com>
2026-03-27 19:16:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,19 +3,16 @@ fetch('ranking.json')
   .then(data => {
     const rankingDiv = document.getElementById("ranking");
 
-    // limpa antes (evita duplicar)
     rankingDiv.innerHTML = "";
 
-    // ordenar por pontos
     data.sort((a, b) => b.pontos - a.pontos);
 
     data.forEach((player, index) => {
       const div = document.createElement("div");
-      div.classList.add("player");
 
       div.innerHTML = `
         <h2>#${index + 1} ${player.nome}</h2>
-        <p>⭐ ${player.pontos} pontos</p>
+        <p>${player.pontos} pontos</p>
       `;
 
       rankingDiv.appendChild(div);
