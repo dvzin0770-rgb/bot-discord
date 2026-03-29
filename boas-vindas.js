7c583a6077f30c2db5cfe3a6472737aeab665900
dvzin0770-rgb <dvzin0770@gmail.com>
2026-03-28 21:17:06 -0300

    Create boas-vindas.js

---
+++ boas-vindas.js
@@ -0,0 +1,37 @@
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = (client) => {
+
+  client.on('guildMemberAdd', async (member) => {
+
+    const canal = member.guild.channels.cache.find(c => c.name === 'geral'); // pode trocar se quiser
+
+    if (!canal) return;
+
+    const embed = new EmbedBuilder()
+      .setColor('#00b0f4')
+      .setTitle(`${member.user.username} | Bem-vindo(a)!`)
+      .setDescription(
+`👋 Olá, seja bem-vindo(a) ao **FROSTVOW ❄️**!
+
+📋 Faça o <#1485275251251220502> para jogar conosco!
+
+🛡️ **Tag do Usuário**
+${member.user.tag}
+(${member.id})
+
+🚨 **Precisando de ajuda?**
+Chame nossa equipe!
+
+👮 **Evite punições!**
+Leia <#1485256438174584893> para evitar problemas!`
+      )
+      .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
+      .setFooter({ text: 'FROSTVOW ❄️ • Todos os direitos reservados' })
+      .setTimestamp();
+
+    canal.send({ embeds: [embed] });
+
+  });
+
+};
